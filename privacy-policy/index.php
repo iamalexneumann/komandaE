6f34412467e43d376b9c15b88718f566150a0934
iamalexneumann <iamalexneumann@yandex.ru>
2023-09-07 18:50:53 +0300

    Adds the privacy policy page

--- privacy-policy/index.php
+++ privacy-policy/index.php
@@ -3,6 +3,13 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Политика конфиденциальности");
 ?>
 
-Text here....
+<?php
+$APPLICATION->IncludeComponent(
+    "sprint.editor:blocks",
+    ".default",
+    Array(
+        "JSON" => \Bitrix\Main\Config\Option::get('askaron.settings', 'UF_PAGE_PRIVACY_POLICY', '')
+    )
+); ?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
