2169e1d93b6b7e1d7164aa2148fbe26d77cecbb9
iamalexneumann <iamalexneumann@yandex.ru>
2023-10-26 16:16:34 +0300

    Adds default disciplines' templates

--- disciplines/index.php
+++ disciplines/index.php
@@ -5,10 +5,11 @@ $APPLICATION->SetPageProperty("title", "Дисциплины | Клуб един
 $APPLICATION->SetTitle("Дисциплины");
 ?>
 
-<? $APPLICATION->IncludeComponent(
+<?php
+$APPLICATION->IncludeComponent(
     "bitrix:news",
-    ".default",
-    array(
+    "disciplines",
+    Array(
         "ADD_ELEMENT_CHAIN" => "Y",
         "ADD_SECTIONS_CHAIN" => "Y",
         "AJAX_MODE" => "N",
@@ -22,72 +23,53 @@ $APPLICATION->SetTitle("Дисциплины");
         "CACHE_TIME" => "36000000",
         "CACHE_TYPE" => "A",
         "CHECK_DATES" => "Y",
+        "COMPOSITE_FRAME_MODE" => "A",
+        "COMPOSITE_FRAME_TYPE" => "AUTO",
         "DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",
         "DETAIL_DISPLAY_BOTTOM_PAGER" => "N",
         "DETAIL_DISPLAY_TOP_PAGER" => "N",
-        "DETAIL_FIELD_CODE" => array(
-            0 => "",
-            1 => "",
-        ),
+        "DETAIL_FIELD_CODE" => array("", ""),
         "DETAIL_PAGER_SHOW_ALL" => "N",
         "DETAIL_PAGER_TEMPLATE" => "",
         "DETAIL_PAGER_TITLE" => "",
-        "DETAIL_PROPERTY_CODE" => array(
-            0 => "ATT_LINKED_SCHEDULE",
-            1 => "ATT_PREVIEW_TEXT",
-            2 => "ATT_DETAIL_TEXT",
-            3 => "MORE_PHOTO",
-            4 => "ATT_LINKED_FEATURES",
-            5 => "ATT_LINKED_PRICES",
-            6 => "",
-        ),
+        "DETAIL_PROPERTY_CODE" => array("ATT_PREVIEW_TEXT", "ATT_DETAIL_TEXT", "MORE_PHOTO", ""),
         "DETAIL_SET_CANONICAL_URL" => "Y",
-        "DISPLAY_BOTTOM_PAGER" => "Y",
-        "DISPLAY_DATE" => "N",
+        "DISPLAY_BOTTOM_PAGER" => "N",
         "DISPLAY_NAME" => "N",
-        "DISPLAY_PICTURE" => "N",
-        "DISPLAY_PREVIEW_TEXT" => "N",
         "DISPLAY_TOP_PAGER" => "N",
-        "FILTER_FIELD_CODE" => array(
-            0 => "",
-            1 => "",
-        ),
-        "FILTER_NAME" => "",
-        "FILTER_PROPERTY_CODE" => array(
-            0 => "",
-            1 => "",
-        ),
+        "FILE_404" => "",
         "HIDE_LINK_WHEN_NO_DETAIL" => "N",
         "IBLOCK_ID" => "1",
         "IBLOCK_TYPE" => "main_content",
         "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
         "LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
-        "LIST_FIELD_CODE" => array(
-            0 => "",
-            1 => "",
-        ),
-        "LIST_PROPERTY_CODE" => array(
-            0 => "ATT_PREVIEW_TEXT",
-            1 => "",
-        ),
+        "LIST_FIELD_CODE" => array("", ""),
+        "LIST_PROPERTY_CODE" => array("ATT_PREVIEW_TEXT", ""),
         "MESSAGE_404" => "",
         "META_DESCRIPTION" => "-",
         "META_KEYWORDS" => "-",
-        "NEWS_COUNT" => "15",
+        "NEWS_COUNT" => "20",
         "PAGER_BASE_LINK_ENABLE" => "N",
         "PAGER_DESC_NUMBERING" => "N",
         "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
         "PAGER_SHOW_ALL" => "N",
         "PAGER_SHOW_ALWAYS" => "N",
-        "PAGER_TEMPLATE" => ".default",
+        "PAGER_TEMPLATE" => "main_pagination",
         "PAGER_TITLE" => "",
         "PREVIEW_TRUNCATE_LEN" => "",
         "SEF_FOLDER" => "/disciplines/",
         "SEF_MODE" => "Y",
+        "SEF_URL_TEMPLATES" => Array(
+            "detail" => "#ELEMENT_CODE#/",
+            "news" => "",
+            "section" => ""
+        ),
         "SET_LAST_MODIFIED" => "Y",
         "SET_STATUS_404" => "Y",
         "SET_TITLE" => "Y",
         "SHOW_404" => "Y",
+        "SHOW_FORM_BLOCK" => "N",
+        "SMALL_CARD_TAG_TITLE" => "2",
         "SORT_BY1" => "SORT",
         "SORT_BY2" => "ACTIVE_FROM",
         "SORT_ORDER1" => "ASC",
@@ -98,19 +80,8 @@ $APPLICATION->SetTitle("Дисциплины");
         "USE_PERMISSIONS" => "N",
         "USE_RATING" => "N",
         "USE_RSS" => "N",
-        "USE_SEARCH" => "N",
-        "USE_SHARE" => "N",
-        "COMPONENT_TEMPLATE" => ".default",
-        "COMPOSITE_FRAME_MODE" => "A",
-        "COMPOSITE_FRAME_TYPE" => "AUTO",
-        "FILE_404" => "",
-        "SEF_URL_TEMPLATES" => array(
-            "news" => "",
-            "section" => "",
-            "detail" => "#ELEMENT_CODE#/",
-        )
-    ),
-    false
+        "USE_SEARCH" => "N"
+    )
 ); ?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/templates/komandae/components/bitrix/news.list/disciplines_list/template.php
@@ -0,0 +1,49 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+/**
+ * @var array $arParams
+ * @var array $arResult
+ * @global CMain $APPLICATION
+ * @global CUser $USER
+ * @global CDatabase $DB
+ * @var array $arLangMessages
+ * @var CBitrixComponentTemplate $this
+ * @var string $templateName
+ * @var string $templateFile
+ * @var string $templateFolder
+ * @var string $parentTemplateFolder
+ * @var string $componentPath
+ * @var array $templateData
+ * @var CBitrixComponent $component
+ */
+$this->setFrameMode(true);
+use Bitrix\Main\Localization\Loc;
+
+$param_small_card_tag_title = $arParams['SMALL_CARD_TAG_TITLE'] ?? '2';
+?>
+<?php if (count($arResult['ITEMS']) > 0): ?>
+<div class="news-list">
+    <?php
+    foreach ($arResult['ITEMS'] as $arItem_key => $arItem):
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem['IBLOCK_ID'], 'ELEMENT_EDIT'));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem['IBLOCK_ID'], 'ELEMENT_DELETE'),
+            [
+                'CONFIRM' => Loc::getMessage('CT_BNL_ELEMENT_DELETE_CONFIRM'),
+            ]
+        );
+    ?>
+    <article class="news-item" id="<?= $this->GetEditAreaId($arItem['ID']) ;?>">
+        <h<?=$param_small_card_tag_title; ?>>
+            <a href="<?= $arItem['DETAIL_PAGE_URL']; ?>"><?= $arItem['NAME']; ?></a>
+        </h<?=$param_small_card_tag_title; ?>>
+    </article>
+    <?php endforeach; ?>
+</div>
+<?php
+if ($arParams['DISPLAY_BOTTOM_PAGER']) {
+    echo $arResult['NAV_STRING'];
+}
+?>
+<?php endif; ?>
